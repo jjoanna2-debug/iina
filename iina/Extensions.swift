7d3d4b9efba391bb8451979500c083bfcd1732a9
Hechen Li <lhc199652@gmail.com>
2026-07-04 22:46:53 -0400

    ui: remove remaining xib components from MainWindow and MiniPlayer

--- iina/Extensions.swift
+++ iina/Extensions.swift
@@ -599,7 +599,6 @@ extension URL {
 
 
 extension NSTextField {
-
   func setHTMLValue(_ html: String) {
     let font = self.font ?? NSFont.systemFont(ofSize: NSFont.systemFontSize)
     let color = self.textColor ?? NSColor.labelColor
@@ -610,8 +609,16 @@ extension NSTextField {
       self.attributedStringValue = str
     }
   }
-
 }
+
+
+extension NSFont {
+  static func monospacedDigitFont(for size: NSControl.ControlSize) -> NSFont {
+    let fontSize = NSFont.systemFontSize(for: size)
+    return NSFont.monospacedDigitSystemFont(ofSize: fontSize, weight: .regular)
+  }
+}
+
 
 extension NSImage {
   var cgImage: CGImage? {
